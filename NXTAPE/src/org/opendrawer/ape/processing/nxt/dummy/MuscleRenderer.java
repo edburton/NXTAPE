18527d812f73c6c1afa41faed961404bb4b9b022
Ed Burton <ed@edburton.net>
2012-12-09 17:31:01 +0000

    deglitched

--- NXTAPE/src/org/opendrawer/ape/processing/nxt/dummy/MuscleRenderer.java
+++ NXTAPE/src/org/opendrawer/ape/processing/nxt/dummy/MuscleRenderer.java
@@ -1,6 +1,7 @@
 package org.opendrawer.ape.processing.nxt.dummy;
 
 import org.opendrawer.ape.darwinianneurodynamics.StatesProvider;
+import org.opendrawer.ape.processing.Renderer;
 import org.opendrawer.ape.processing.StatesProviderRenderer;
 
 import processing.core.PGraphics;
@@ -16,16 +17,14 @@ public class MuscleRenderer extends StatesProviderRenderer {
 	@Override
 	public void draw(PGraphics g) {
 		super.draw(g);
-		// g.strokeWeight(Renderer.lineWidth);
-		// float xc = x + width / 2;
-		// float value = (float) muscle.getCurrentRestLength();
-		//
-		// float mh = ((1.2f - value) / 4) * width;
-		// g.stroke(255, 128, 128);
-		// g.fill(250, 100, 100);
-		// g.ellipse(xc - mh, (y + height) - (height * value), xc + mh, y +
-		// height);
+		g.strokeWeight(Renderer.lineWidth);
+		float xc = x + width / 2;
+		float value = (float) muscle.getCurrentRestLength();
 
+		float mh = ((1.2f - value) / 4) * width;
+		g.stroke(255, 128, 128);
+		g.fill(250, 100, 100);
+		g.ellipse(xc - mh, (y + height) - (height * value), xc + mh, y + height);
 	}
 
 	@Override
